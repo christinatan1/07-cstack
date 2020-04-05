e79e5945063407dd454724c0a8f85633bbda9195
Christina Tan <christy11204@gmail.com>
2020-04-05 17:44:08 -0400

    progress

---
+++ main.py
@@ -0,0 +1,15 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+color = [ 255, 255, 255 ]
+edges = []
+polygons = []
+t = new_matrix()
+ident(t)
+csystems = [ t ]
+
+parse_file( 'myscript', edges, polygons, csystems, screen, color )
